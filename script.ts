52dac45010b77ef9dc1159cd3f5140343a9decdd
Lee Junmo <leejunmo84@gmail.com>
2022-11-22 03:05:06 +0900

    Add submit event

--- script.ts
+++ script.ts
@@ -16,15 +16,33 @@ class Board extends React.Component {
         distractors: [],
     }
   }
+  
+  updateList(e){
+    e.preventDefault();
+    let new_answers, new_distractors;
+    
+    if (e.target.type.value == 0){
+      new_answers = [...this.state.answers, e.target.text.value];
+      new_distractors = [...this.state.distractors];
+    } else {
+      new_answers = [...this.state.answers];
+      new_distractors = [...this.state.distractors, e.target.text.value];
+    }
+    
+    this.setState({
+      answers: new_answers,
+      distractors: new_distractors,
+    })
+  }
+  
   render() {
     return (
       <div>
         <div className="list-scroll">
           <div className="list-wrapper">
             <div className="list-title">Answers</div>
-            {
-                this.state.answers.map(v => (<List value={v}/>))
-            }
+            {this.state.answers.map(v => (<List value={v}/>))}
+               
           </div>
           <div className="list-wrapper">
             <div className="list-title">Distractors</div>
@@ -33,25 +51,25 @@ class Board extends React.Component {
             }
           </div>
         </div>
-        <div className="list-insert">
+        <form className="list-insert" onSubmit={e => this.updateList(e)}>
           <div>Create a New Option</div>
           <div>
               <div>
-                <input placeholder="Type to create option..."></input>
+                <input placeholder="Type to create option..." name="text"></input>
               </div>
-              <div>
-                 <label name="answer">Answer</label>
-                 <input type="radio" name="answer"></input>
-              </div>
-              <div>
-                 <label name="answer">Answer</label>
-                 <input type="radio" name="answer"></input>
-              </div>
+              <label>
+                Answer
+                <input type="radio" name="type" id="answer" value="0"></input>
+              </label>
+              <label>
+                Distractors
+                <input type="radio" name="type" id="distractors" value="1"></input>
+              </label>
           </div>
           <div>
             <button>Submit</button>
           </div>
-        </div>
+        </form>
       </div>
     );
   }
