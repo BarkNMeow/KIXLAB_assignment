8b2c55394d56acee73b1a70d2e03c977dfee6b6a
Lee Junmo <leejunmo84@gmail.com>
2022-11-22 04:06:00 +0900

    Add AJAX calls

--- script.ts
+++ script.ts
@@ -1,38 +1,80 @@
-class List extends React.Component {  
-  render() {
+function List(props){
     return (
        <div>
-          {this.props.value}
+          {props.value}
        </div>
     )
-  }
 }
 
 class Board extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-        answers: [],
-        distractors: [],
+      answers: [],
+      distractors: [],
     }
+  }
+  
+  componentDidMount() {
+    fetch('http://kuiz.kixlab.org:8080/getOptions')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            answers: result.answers,
+            distractors: result.distractors,
+          })
+        },
+        (error) => {
+          console.log("Failed to fetch");
+        }
+      )
   }
   
   updateList(e){
     e.preventDefault();
     let new_answers, new_distractors;
     
-    if (e.target.type.value == 0){
-      new_answers = [...this.state.answers, e.target.text.value];
-      new_distractors = [...this.state.distractors];
-    } else {
-      new_answers = [...this.state.answers];
-      new_distractors = [...this.state.distractors, e.target.text.value];
-    }
+    const input = e.target.text.value;
+    const isAnswer = (e.target.type.value == 0);
     
-    this.setState({
-      answers: new_answers,
-      distractors: new_distractors,
-    })
+    if(input === '')
+      return
+    
+    fetch('http://kuiz.kixlab.org:8080/submitOption', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        optionLabel: input,
+        isAnswer: isAnswer,
+      }),
+  }).then(res => res.json())
+    .then(
+        (result) => {
+          if(!result.success){
+            alert('response was not successful!');
+            return;
+          }
+          
+          if (isAnswer){
+            new_answers = [...this.state.answers, input];
+            new_distractors = [...this.state.distractors];
+          } else {
+            new_answers = [...this.state.answers];
+            new_distractors = [...this.state.distractors, input];
+          }
+          
+          this.setState({
+            answers: new_answers,
+            distractors: new_distractors,
+          })
+        },
+        (error) => {
+          console.log("Failed to fetch");
+        }
+      )
   }
   
   render() {
@@ -41,13 +83,13 @@ class Board extends React.Component {
         <div className="list-scroll">
           <div className="list-wrapper">
             <div className="list-title">Answers</div>
-            {this.state.answers.map(v => (<List value={v}/>))}
+            {this.state.answers.map((v, i) => (<List key={i} value={v}/>))}
                
           </div>
           <div className="list-wrapper">
             <div className="list-title">Distractors</div>
             {
-                this.state.distractors.map(v => (<List value={v}/>))
+                this.state.distractors.map((v, i) => (<List key={i} value={v}/>))
             }
           </div>
         </div>
@@ -59,7 +101,7 @@ class Board extends React.Component {
               </div>
               <label>
                 Answer
-                <input type="radio" name="type" id="answer" value="0"></input>
+                <input type="radio" name="type" id="answer" value="0" defaultChecked="checked"></input>
               </label>
               <label>
                 Distractors
